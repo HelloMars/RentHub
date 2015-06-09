00ebb67eef2683ca88d1cc8dc74a83898881bcc7
surface <zhumeng1989@gmail.com>
2015-06-09 21:00:32 +0800

    Test

--- cloud.py
+++ cloud.py
@@ -1,5 +1,10 @@
 # coding: utf-8
 
+import time
+import sched
+import requests
+from lxml import html
+
 from leancloud import Engine
 
 from app import app
@@ -14,3 +19,30 @@ def hello(**params):
         return 'Hello, {}!'.format(params['name'])
     else:
         return 'Hello, LeanCloud!'
+
+
+def load_tasks():
+    s = sched.scheduler(time.time, time.sleep)
+    _check(5, s)
+    s.run()
+
+
+def _check(interval, sc):
+    print time.time(), "Start checking ..."
+    r = requests.get("http://www.zufangzi.com/area/1b90B-22200/1/")
+    if r.status_code == 200:
+        tree = html.fromstring(r.text)
+        houses = tree.xpath('//div[@id="houseList"]/div[@class="listCon wid1000"]')
+        num = 0
+        for house in houses:
+            num += 1
+            print 'House #', num
+            for block in house.iterchildren():
+                if block.get('class') == 'listCon_Img':
+                    for link in block.iterlinks():
+                        if link[1] == 'src':
+                            print link[2]
+                elif block.get('class') == 'listCon_con':
+                    for ele in block.xpath('//p[@class="listcon_1"]/em/a'):
+                        print ele.text_content()
+    sc.enter(interval, 1, _check, (interval, sc,))
--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,8 @@ import os
 import leancloud
 from wsgiref import simple_server
 
+import cloud
 from app import app
-from cloud import engine
 
 APP_ID = os.environ['LC_APP_ID']
 MASTER_KEY = os.environ['LC_APP_MASTER_KEY']
@@ -15,11 +15,12 @@ PORT = int(os.environ['LC_APP_PORT'])
 
 leancloud.init(APP_ID, master_key=MASTER_KEY)
 
-application = engine
+application = cloud.engine
 
 
 if __name__ == '__main__':
     # 只在本地开发环境执行的代码
     app.debug = True
+    cloud.load_tasks()
     server = simple_server.make_server('localhost', PORT, application)
     server.serve_forever()
